ac78bf8995f582de07bf0ddd92314730840e37f9
kooshr <kaushalramesh1@gmail.com>
2022-11-20 01:29:07 -0800

    added basic homescreen functionality

---
+++ Home.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { view, SafeArea, FlatList, Text } from 'native-react'
+
+const Home = () => {
+    return(
+        <Text>Home</Text>
+    )
+}
+
+export default Home
