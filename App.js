8ae7cbf2b23097a74ea6757e5636e9a2855cdcca
Rithwik <rithwik.vaidun@gmail.com>
2022-11-19 20:18:49 -0800

    connected to firebase

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View, Text } from "react-native";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "/Users/rithwikvaidun/food-delivery/firebase.js";
 
 export default function App() {
+  const test = async (x) => {
+    console.log(x);
+    await setDoc(doc(db, "charachters", "mario"), {
+      employment: "rithwik sad",
+      outfit: "red",
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start  rathik on your app!</Text>
+      <Text>Open up App.js to start rathik ? your apip!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={() => test(8)}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
