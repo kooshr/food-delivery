52840aefd521f90507e343f20da8f0516a514c74
rathikm <63566689+rathikm@users.noreply.github.com>
2022-11-19 20:16:49 -0800

    basic verification code box

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState, useEffect } from "react";
 export default function App() {
+  const [code, setCode] = useState("hello");
+  //const [submitted, setSubmitStatus] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter your verification code</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(value) => setCode(value)}
+      />
+      <Button title="submit" onPress={() => console.log(code)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
