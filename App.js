27940b657587b3e0adf6cb4be639fe56b4f79438
kooshr <kaushalramesh1@gmail.com>
2022-11-20 03:01:04 -0800

    home page functionality added

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name = "Home" component = {Home} />
+        <Stack.Screen name = "FoodLine" component = {Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,19 +1,38 @@
 import * as React from 'react';
-import { View,Text,StyleSheet,Image,SafeAreaView } from 'react-native'; 
+import { useState } from 'react';
+import { View,Text,StyleSheet,Image,SafeAreaView, Button } from 'react-native'; 
+import SearchBar from 'react-native-search-bar';
 import colors from './colors.js';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Feather from 'react-native-vector-icons/Feather'
+import {db} from '../firebase.js';
+import { TextInput } from 'react-native-gesture-handler';
 
 Feather.loadFont();
 
+const test = async (x) => {
+    console.log(x);
+    await setDoc(doc(db, "codes", "user1"), {
+      employment: "Code1",
+      outfit: "890OLF13",
+    });
+  };
+
 const Home = () => {
+    
     return(
+        
         <View style={styles.container}>
             <SafeAreaView>
                 <View style={styles.headerWrapper}>
                     <View style={styles.SearchBgLight}></View>
                     <View style={styles.SearchBgDarkRight}></View>
                     <View style={styles.SearchBgDarkLeft}></View>
+                    <Feather name = 'search' size={24} style={styles.searchIcon}/>
+                    <TextInput
+                        style={styles.searchButton}
+                        placeholder={'Search'}
+                    />
                 </View>
             </SafeAreaView>
             <SafeAreaView>
@@ -41,6 +60,24 @@ const styles = StyleSheet.create({
         backgroundColor:colors.background,
         flex:1,
     },
+    searchButton:{
+        position:'absolute',
+        flex:1,
+        top:23,
+        left:115,
+        height:30,
+        borderBottomWidth:1,
+        borderBottomColor:colors.text,
+        width:230,
+
+    },
+    searchIcon:{
+        position:'relative',
+        top:-103,
+        left:40,
+        color:colors.text,
+
+    },
 
     SearchBgLight: {
         position:'relative',
@@ -49,7 +86,10 @@ const styles = StyleSheet.create({
         left:10,
         top:15,
         backgroundColor: colors.tint,
-        borderRadius:40,        
+        borderRadius:40,     
+        shadowOpacity:.3,
+        shadowOffset: {width: 10, height:10},
+           
     },
 
     SearchBgDarkRight: {
@@ -75,17 +115,19 @@ const styles = StyleSheet.create({
     FeaturedBG:{
         position:'absolute',
         left:10,
-        top:- 50,
+        top:- 70,
         width:370,
         height:230,
         backgroundColor: colors.tint,
         borderRadius:40,
+        shadowOpacity:.3,
+        shadowOffset: {width: 10, height:10},
     }, 
 
     FeaturedBGInner:{
         position:'relative',
         left:20,
-        top: -40,
+        top: -60,
         width:350,
         height:210,
         backgroundColor: colors.search,
@@ -95,12 +137,14 @@ const styles = StyleSheet.create({
     menuTabs: {
         position:'relative',
         left:10,
-        top:-10,
+        top:-30,
         width:370,
         height: 110,
         backgroundColor:colors.tint,
         borderRadius:40,
-        marginBottom:15,
+        marginBottom:20,
+        shadowOpacity:.3,
+        shadowOffset: {width: 10, height:10},
     },
 
 });
